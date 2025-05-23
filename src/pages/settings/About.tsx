5b52a5b99244241736e28e1eb2d54e968e5a4cb1
RAZGRIZ219104 <razgriz219104@gmail.com>
2025-05-23 19:04:03 +0200

    feat: add social info

--- src/pages/settings/About.tsx
+++ src/pages/settings/About.tsx
@@ -1,134 +1,136 @@
 import { useTranslation } from "react-i18next";
-
 import { Box, Button, Divider, Stack } from "@mui/material";
-
 import { CONST_TYMT_LINKS } from "../../const/tymtConsts";
-
 import { openLink } from "../../lib/helper/TauriHelper";
-
-import backIcon from "../../assets/setting/BackIcon.svg";
-import externalIcon from "../../assets/setting/ExternalLink.svg";
-import twitterIcon from "../../assets/setting/TwitterIcon.svg";
-import discordIcon from "../../assets/setting/DiscordIcon.svg";
 import TooltipComponent from "../../components/home/TooltipComponent";
+import backIcon from "../../assets/setting/BackIcon.svg";
+import followLink from "../../assets/setting/FollowLink.svg";
+import followX from "../../assets/setting/FollowX.svg";
+import followDiscord from "../../assets/setting/FollowDiscord.svg";
+import followTelegram from "../../assets/setting/FollowTelegram.svg";
+import followMedium from "../../assets/setting/FollowMedium.svg";
+import followInstagram from "../../assets/setting/FollowInstagram.svg";
+import followFacebook from "../../assets/setting/FollowFacebook.svg";
+import externalIcon from "../../assets/setting/ExternalIcon.svg";
 
 export interface IPropsAbout {
   view: string;
   setView: (_: string) => void;
 }
 
+const linkButtons = [
+  {
+    key: "documentation",
+    labelKey: "set-51_documentation",
+    url: CONST_TYMT_LINKS.documentation,
+  },
+  {
+    key: "policy",
+    labelKey: "set-52_privacy-policy",
+    url: CONST_TYMT_LINKS.policy,
+  },
+];
+
+const socialLinks = [
+  { key: "website", icon: followLink, url: CONST_TYMT_LINKS.website },
+  {
+    key: "twitter",
+    icon: followX,
+    url: CONST_TYMT_LINKS.twitter,
+  },
+  {
+    key: "discord",
+    icon: followDiscord,
+    url: CONST_TYMT_LINKS.discord,
+  },
+  {
+    key: "telegram",
+    icon: followTelegram,
+    url: CONST_TYMT_LINKS.telegram,
+  },
+  {
+    key: "medium",
+    icon: followMedium,
+    url: CONST_TYMT_LINKS.medium,
+  },
+  {
+    key: "instagram",
+    icon: followInstagram,
+    url: CONST_TYMT_LINKS.instagram,
+  },
+  {
+    key: "facebook",
+    icon: followFacebook,
+    url: CONST_TYMT_LINKS.facebook,
+  },
+];
+
 const About = ({ view, setView }: IPropsAbout) => {
   const { t } = useTranslation();
 
-  return (
-    <>
-      {view === "about" && (
-        <>
-          <Stack direction={"column"}>
-            <Stack flexDirection={"row"} justifyContent={"flex-start"} gap={"10px"} alignItems={"center"} textAlign={"center"} className="p-20">
-              <Button className={"setting-back-button"} onClick={() => setView("general")}>
-                <Box component={"img"} src={backIcon}></Box>
-              </Button>
-              <Box className="fs-h3 white">{t("set-50_about")}</Box>
-            </Stack>
-            <Divider variant="middle" sx={{ backgroundColor: "#FFFFFF1A" }} />
-            <Stack direction={"column"} justifyContent={""}>
-              <TooltipComponent placement="bottom" text={CONST_TYMT_LINKS.documentation}>
-                <Button
-                  className="common-btn"
-                  sx={{ padding: "20px" }}
-                  onClick={() => {
-                    openLink(CONST_TYMT_LINKS.documentation);
-                  }}
-                >
-                  <Stack direction={"row"} justifyContent={"space-between"} textAlign={"center"}>
-                    <Box className="fs-h4 white">{t("set-51_documentation")}</Box>
-                    <Box className="center-align">
-                      <img src={externalIcon} />
-                    </Box>
-                  </Stack>
-                </Button>
-              </TooltipComponent>
+  if (view !== "about") return null;
 
-              <Divider variant="fullWidth" sx={{ backgroundColor: "#FFFFFF1A" }} />
-              <TooltipComponent placement="bottom" text={CONST_TYMT_LINKS.policy}>
-                <Button
-                  className="common-btn"
-                  sx={{ padding: "20px" }}
-                  onClick={() => {
-                    openLink(CONST_TYMT_LINKS.policy);
-                  }}
-                >
-                  <Stack direction={"row"} justifyContent={"space-between"} textAlign={"center"}>
-                    <Box className="fs-h4 white">{t("set-52_privacy-policy")}</Box>
-                    <Box className="center-align">
-                      <img src={externalIcon} />
-                    </Box>
-                  </Stack>
-                </Button>
-              </TooltipComponent>
-              <Divider variant="fullWidth" sx={{ backgroundColor: "#FFFFFF1A" }} />
-              {/* <Button
-                className="common-btn"
-                sx={{ padding: "20px" }}
-                onClick={() => {
-                  openLink(tymtlinks.links.termsofservice);
-                }}
-              >
-                <Stack direction={"row"} justifyContent={"space-between"} textAlign={"center"}>
-                  <Box className="fs-h4 white">{t("set-53_terms-service")}</Box>
+  return (
+    <Stack direction="column">
+      <Stack flexDirection="row" justifyContent="flex-start" gap="10px" alignItems="center" textAlign="center" className="p-20">
+        <Button className="setting-back-button" onClick={() => setView("general")}>
+          <Box component="img" src={backIcon} />
+        </Button>
+        <Box className="fs-h3 white">{t("set-50_about")}</Box>
+      </Stack>
+      <Divider variant="middle" sx={{ backgroundColor: "#FFFFFF1A" }} />
+
+      <Stack direction="column">
+        {linkButtons.map(({ key, labelKey, url }) => (
+          <TooltipComponent placement="bottom" text={url} key={key}>
+            <>
+              <Button className="common-btn" sx={{ padding: "20px" }} onClick={() => openLink(url)}>
+                <Stack direction="row" justifyContent="space-between" textAlign="center">
+                  <Box className="fs-h4 white">{t(labelKey)}</Box>
                   <Box className="center-align">
-                    <img src={externalIcon} />
+                    <img src={externalIcon} alt="" />
                   </Box>
                 </Stack>
-              </Button> */}
+              </Button>
               <Divider variant="fullWidth" sx={{ backgroundColor: "#FFFFFF1A" }} />
-              <Stack direction={"column"} justifyContent={"flex-start"} textAlign={"left"} gap={"20px"} padding={"20px"}>
-                <Box className="fs-h4 white">{t("set-54_follow-us")}</Box>
-                <Stack direction={"row"} justifyContent={"flex-start"} gap={"10px"}>
-                  <TooltipComponent placement="bottom" text={CONST_TYMT_LINKS.twitter}>
-                    <Button
-                      className="button_navbar_common"
-                      sx={{ padding: 0 }}
-                      onClick={() => {
-                        openLink(CONST_TYMT_LINKS.twitter);
-                      }}
-                    >
-                      <Box className="center-align">
-                        <img src={twitterIcon} />
-                      </Box>
-                    </Button>
-                  </TooltipComponent>
-                  <TooltipComponent placement="bottom" text={CONST_TYMT_LINKS.discord}>
-                    <Button
-                      className="button_navbar_common"
-                      sx={{ padding: 0 }}
-                      onClick={() => {
-                        openLink(CONST_TYMT_LINKS.discord);
-                      }}
-                    >
-                      <Box className="center-align">
-                        <img src={discordIcon} />
-                      </Box>
-                    </Button>
-                  </TooltipComponent>
-                </Stack>
-              </Stack>
-            </Stack>
+            </>
+          </TooltipComponent>
+        ))}
+
+        <Stack direction="column" justifyContent="flex-start" textAlign="left" gap="20px" padding="20px">
+          <Box className="fs-h4 white">{t("set-54_follow-us")}</Box>
+          <Stack direction="row" justifyContent="flex-start" gap="10px">
+            {socialLinks.map(({ key, icon, url }) => (
+              <TooltipComponent placement="bottom" text={url} key={key}>
+                <Button
+                  className="button_navbar_common"
+                  sx={{ padding: 0 }}
+                  onClick={() => {
+                    console.log(url);
+                    openLink(url);
+                  }}
+                >
+                  <Box className="center-align">
+                    <img src={icon} alt={key} />
+                  </Box>
+                </Button>
+              </TooltipComponent>
+            ))}
           </Stack>
-          <Box
-            className="fs-16-regular light"
-            sx={{
-              position: "absolute",
-              bottom: "40px",
-              left: "16px",
-            }}
-          >
-            {/* {`${t("set-84_app-version")} v${tymt_version}`} */}
-          </Box>
-        </>
-      )}
-    </>
+        </Stack>
+      </Stack>
+
+      <Box
+        className="fs-16-regular light"
+        sx={{
+          position: "absolute",
+          bottom: "40px",
+          left: "16px",
+        }}
+      >
+        {/* {`${t("set-84_app-version")} v${tymt_version}`} */}
+      </Box>
+    </Stack>
   );
 };
 
