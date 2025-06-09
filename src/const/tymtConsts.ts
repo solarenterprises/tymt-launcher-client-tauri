eda85fca64ca649f8a302c3494a2249f3714d1f6
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2025-06-09 15:29:36 -0400

    fix: update tymt video url

--- src/const/tymtConsts.ts
+++ src/const/tymtConsts.ts
@@ -18,7 +18,7 @@ export const CONST_TYMT_LINKS = {
   twitter: "https://twitter.com/tymt_com/",
   discord: "https://discord.solar.org/",
   solarcard: "https://solarcard.app/",
-  youtube: "https://youtube.com/watch?v=Q6KL6rrbmNg",
+  youtube: "https://tymt-video.up.railway.app/tymt.mp4",
   telegram: "https://t.me/Solar/",
   medium: "https://blog.solar.org/",
   instagram: "https://www.instagram.com/solarnetworkofficial/",
